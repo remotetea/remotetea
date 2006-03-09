672664790ddc97dec328ef33f5b8cf97e2b18fb3
Harald Albrecht <harald@plt.rwth-aachen.de>
2006-03-09 20:54:28 +0000

    fixed dispatcher code generation where the wrong ONC/RPC error was returned: now returns proper program unavailable instead of procedure unavailable if a particular program number is not supported

--- src/org/acplt/oncrpc/apps/jrpcgen/jrpcgen.java
+++ src/org/acplt/oncrpc/apps/jrpcgen/jrpcgen.java
@@ -81,7 +81,7 @@ public class jrpcgen {
     /**
      * Current version of jrpcgen.
      */
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
 
     /**
      * A remote procedure has no parameters and thus needs to use the
@@ -2221,7 +2221,7 @@ public class jrpcgen {
         }
 
         out.println("        } else {");
-        out.println("            call.failProcedureUnavailable();");
+        out.println("            call.failProgramUnavailable();");
         out.println("        }");
         out.println("    }");
         out.println();
